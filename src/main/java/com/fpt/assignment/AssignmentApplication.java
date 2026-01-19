9ccc95e69cfbebd021453bc2a3969226e122f2d5
thaiHocng <ngthaihoc.vn@gmail.com>
2026-01-19 10:05:40 +0700

    map href

--- src/main/java/com/fpt/assignment/AssignmentApplication.java
+++ src/main/java/com/fpt/assignment/AssignmentApplication.java
@@ -8,7 +8,7 @@ public class AssignmentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AssignmentApplication.class, args);
-        System.out.println("Running...");
+        System.out.println("Running successfully");
     }
 
 }
