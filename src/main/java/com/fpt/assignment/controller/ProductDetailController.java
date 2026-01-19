e1783b492030e8da1b11c9d26abd2c549da4f215
thuyvy247 <thuyvy25012006@gmail.com>
2026-01-19 10:41:59 +0700

    Gui them file Controller

--- src/main/java/com/fpt/assignment/controller/ProductDetailController.java
+++ src/main/java/com/fpt/assignment/controller/ProductDetailController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/product-detail")
 public class ProductDetailController {
+    
     @GetMapping
     public String viewProductDetail() {
         return "views/product/productDetail";
