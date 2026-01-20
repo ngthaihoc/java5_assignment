48b9ef9ab30ffe341ee537bba62ae7a7a494beb9
DevHieu <hieudd2090@gmail.com>
2026-01-20 13:09:33 +0700

    Hoàn thành các chức nawgn login, register, verify, đổi mật khẩu. Lưu đăng nhập bằng cookie và session. Làm filer để xác thực đăng nhập ban đầu và interceptor để xác thực user thwuwongf hay admin

--- src/main/java/com/fpt/assignment/controller/AccountController.java
+++ src/main/java/com/fpt/assignment/controller/AccountController.java
@@ -3,12 +3,9 @@ package com.fpt.assignment.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AccountController {
-
 
     @GetMapping("/account")
     public String viewAccount(Model model) {
@@ -18,14 +15,12 @@ public class AccountController {
         return "views/account";
     }
 
-
     @GetMapping("/account/profile")
     public String viewProfile(Model model) {
         model.addAttribute("userFullname", "Nguyễn Học");
         model.addAttribute("currentTab", "profile");
         return "views/account";
     }
-
 
     @GetMapping("/account/orders")
     public String viewOrders(Model model) {
@@ -34,14 +29,12 @@ public class AccountController {
         return "views/account";
     }
 
-
     @GetMapping("/account/change-password")
     public String viewChangePassword(Model model) {
         model.addAttribute("userFullname", "Nguyễn Học");
         model.addAttribute("currentTab", "password");
         return "views/account";
     }
-
 
     @GetMapping("/account/address")
     public String viewAddress(Model model) {
