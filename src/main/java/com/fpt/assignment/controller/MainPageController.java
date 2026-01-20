4dd303e774e0c0b442e00f515a94b8fd5318525b
thaiHocng <ngthaihoc.vn@gmail.com>
2026-01-21 00:03:22 +0700

    done biding data one way main page

--- src/main/java/com/fpt/assignment/controller/MainPageController.java
+++ src/main/java/com/fpt/assignment/controller/MainPageController.java
@@ -1,11 +1,17 @@
 package com.fpt.assignment.controller;
 
+import com.fpt.assignment.entity.Book;
 import com.fpt.assignment.repository.BookRepository;
+import com.fpt.assignment.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 
@@ -14,10 +20,25 @@ public class MainPageController {
     @Autowired
     BookRepository bookRepo;
 
-    @RequestMapping("/home")
-    public String mainPage(Model model) {
+    @Autowired
+    CategoryRepository categoryRepo;
 
+    @RequestMapping("/home")
+    public String mainPage(Model model, @RequestParam("cateid") Optional<Long> cateid) {
+//      Top sách
         model.addAttribute("topBooks", bookRepo.findAll(PageRequest.of(0, 8)).getContent());
+
+//      thể loại
+        model.addAttribute("categories", categoryRepo.findAll());
+
+        List<Book> list;
+        if (cateid.isPresent()) {
+            list = list = bookRepo.findByCategoryId(cateid.get(), PageRequest.of(0, 8));
+        } else {
+            list = bookRepo.findAll(PageRequest.of(0, 8)).getContent();
+        }
+
+        model.addAttribute("booksForYou", list);
         return "/components/content";
     }
 }
--- src/main/java/com/fpt/assignment/repository/BookRepository.java
+++ src/main/java/com/fpt/assignment/repository/BookRepository.java
@@ -1,11 +1,16 @@
 package com.fpt.assignment.repository;
 
 import com.fpt.assignment.entity.Book;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Override
     List<Book> findAll();
+
+    @Query("SELECT b FROM Book b WHERE b.category.id = ?1")
+    List<Book> findByCategoryId(Long cateid, PageRequest of);
 }
