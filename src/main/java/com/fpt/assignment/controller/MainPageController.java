7b3a5e160708462e14201d3e2800c4defae1bd6c
thaiHocng <ngthaihoc.vn@gmail.com>
2026-01-20 22:39:54 +0700

    đã xong top 8 selling, chưa ổn vì database chưa đủ sẽ update sau

--- src/main/java/com/fpt/assignment/controller/MainPageController.java
+++ src/main/java/com/fpt/assignment/controller/MainPageController.java
@@ -1,15 +1,23 @@
 package com.fpt.assignment.controller;
 
+import com.fpt.assignment.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 public class MainPageController {
 
-    @RequestMapping("/home")
-    public String mainPage() {
+    @Autowired
+    BookRepository bookRepo;
 
+    @RequestMapping("/home")
+    public String mainPage(Model model) {
+
+        model.addAttribute("topBooks", bookRepo.findAll(PageRequest.of(0, 8)).getContent());
         return "/components/content";
     }
 }
--- src/main/java/com/fpt/assignment/repository/BookRepository.java
+++ src/main/java/com/fpt/assignment/repository/BookRepository.java
@@ -3,6 +3,9 @@ package com.fpt.assignment.repository;
 import com.fpt.assignment.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import java.util.List;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+    @Override
+    List<Book> findAll();
 }
